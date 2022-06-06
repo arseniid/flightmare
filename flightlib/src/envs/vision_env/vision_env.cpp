ce35d0be90e243e0d66ebb7f5a157a1a082ac439
Arsenii Dunaev <dunaev.arseniy@gmail.com>
2022-06-06 19:25:53 +0200

    Improve terminal reward computation: 1. Add collision penalty 2. Penalize timeout more 3. Going out of the bounding box should terminate the rollout 4. WIP: Add goal position of 60m

--- flightlib/src/envs/vision_env/vision_env.cpp
+++ flightlib/src/envs/vision_env/vision_env.cpp
@@ -329,14 +329,14 @@ bool VisionEnv::computeReward(Ref<Vector<>> reward) {
 }
 
 bool VisionEnv::isTerminalState(Scalar &reward) {
-  // if (is_collision_) {
-  //   reward = -1.0;
-  //   return true;
-  // }
+  if (is_collision_) {
+    reward = -1.0;
+    return true;
+  }
 
   // simulation time out
   if (cmd_.t >= max_t_ - sim_dt_) {
-    reward = 0.0;
+    reward = -1.0;
     return true;
   }
 
@@ -351,6 +351,11 @@ bool VisionEnv::isTerminalState(Scalar &reward) {
                  quad_state_.x(QS::POSZ) <= world_box_[5] - safty_threshold;
   if (!x_valid || !y_valid || !z_valid) {
     reward = -1.0;
+    return true;
+  }
+
+  if (quad_state_.x(QS::POSX) >= 60) {
+    reward = 1.0;
     return true;
   }
   return false;
